53c9622c458c3de8f50e20f6c8fcd052f7b52cf0
Ilya Uhanev <ilya.uhanev@gmail.com>
2026-03-08 17:03:03 +0000

    Added GUI logic for task page

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,6 +1,41 @@
 import styles from "./task.module.css";
+import { useState, type ChangeEvent } from "react";
+
+type Status = "Не решено" | "Решено верно" | "Решено неверно";
 
 const Task = () => {
+  const [currentTaskId, setCurrentTaskId] = useState<number>(0);
+  const [isAnswerShown, setIsAnswerShown] = useState<boolean>(false);
+  const [answer, setAnswer] = useState<string>("18");
+  const [status, setStatus] = useState<Status>("Не решено");
+  const [userAnswer, setUserAnswer] = useState<string>("");
+
+  const handleDecreaseId = (id: number) => {
+    if (id > 0) {
+      setCurrentTaskId(id - 1);
+    }
+  };
+
+  const handleIncreaseId = (id: number) => {
+    if (id < 4) {
+      setCurrentTaskId(id + 1);
+    }
+  };
+
+  const handleSubmitAnswer = (value: string) => {
+    if (value == "") {
+      return;
+    } else if (value == answer) {
+      setStatus("Решено верно");
+    } else {
+      setStatus("Решено неверно");
+    }
+  };
+
+  const handleChangeAnswer = (event: ChangeEvent<HTMLInputElement>) => {
+    setUserAnswer(event.target.value);
+  };
+
   return (
     <>
       <div className="container">
@@ -10,15 +45,46 @@ const Task = () => {
             0,5 ч?
           </h4>
           <span className={styles.span}>Введите ответ:</span>
-          <input type="text" className={styles.input} />
+          <input
+            type="text"
+            className={styles.input}
+            value={userAnswer}
+            onChange={handleChangeAnswer}
+          />
           <div className={styles.buttons}>
-            <button className={styles.complete}>Подтвердить</button>
-            <button className={styles.show_answer}>Показать ответ</button>
-            <span className={styles.answer}></span>
+            <button
+              className={styles.complete}
+              onClick={() => handleSubmitAnswer(userAnswer)}
+            >
+              Подтвердить
+            </button>
+            <button
+              className={styles.show_answer}
+              onClick={() => setIsAnswerShown(!isAnswerShown)}
+            >
+              {isAnswerShown ? "Скрыть ответ" : "Показать ответ"}
+            </button>
+            <span className={styles.answer}>{isAnswerShown && answer}</span>
           </div>
+          <span
+            className={
+              status == "Решено верно"
+                ? styles.right
+                : status == "Решено неверно"
+                  ? styles.wrong
+                  : styles.neutral
+            }
+          >
+            {status}
+          </span>
+
           <div className={styles.nav_buttons}>
-            <button>{"< назад"}</button>
-            <button>{"далее >"}</button>
+            <button onClick={() => handleDecreaseId(currentTaskId)}>
+              {"< назад"}
+            </button>
+            <button onClick={() => handleIncreaseId(currentTaskId)}>
+              {"далее >"}
+            </button>
           </div>
         </main>
       </div>
